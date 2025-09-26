580ae012e4b36ae0e849bc6c31cf858e4ed12f16
RIOKOWI <achyarrio2004@gmail.com>
2025-09-26 20:05:25 +0700

    FIELD :

--- README.md
+++ README.md
@@ -11,9 +11,16 @@
 contoh di file :
 
 class.dart
-LINE 3-6
+LINE 3-4
 
 ## OBJECT
+
+contoh di file :
+
+class.dart
+LINE 10-14
+
+## FIELD
 
 contoh di file :
 
--- bin/class.dart
+++ bin/class.dart
@@ -2,14 +2,21 @@
 
 //class
 class Person{
-
+  // field
+  String name = 'Guest';
+  String? address;
+  final String country = 'Indonesia';
 }
 
 void main()
 {
   // object
   var person1 = Person();
-  print(person1);
+  // field
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
+
   Person person2 = Person();
   print(person2);
 }
