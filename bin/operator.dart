0153aa8d757091dd935e15f8c11e8f6a4e939af7
RIOKOWI <achyarrio2004@gmail.com>
2025-09-27 08:08:40 +0700

    EXTENSION METHOD : buat operator

---
+++ bin/operator.dart
@@ -0,0 +1,14 @@
+class Orange {
+  int quantity = 0;
+
+  Orange operator +(Orange orange){
+    var result = Orange();
+    result.quantity = quantity + orange.quantity;
+    return result;
+  }
+}
+
+void main ()
+{
+  
+}
