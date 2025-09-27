4f47a8bcc043977a5f1b5fcbd353cd726460e2fe
RIOKOWI <achyarrio2004@gmail.com>
2025-09-28 06:57:52 +0700

    CONSTANT CONSTRUCTOR : panggil constant constructor

--- bin/constant_constructor.dart
+++ bin/constant_constructor.dart
@@ -6,7 +6,12 @@ class Immutable{
 }
 
 void main(){
-  var constant = Immutable(6, 8);// panggil constant constructor
-  print(constant.x);// panggil constant constructor
-  print(constant.y);// panggil constant constructor
+  
+  var point1 = Immutable(10, 10);
+  var point2 = Immutable(10, 10);
+  print(point1 == point2); // false
+
+  var p1 = const Immutable(6, 9); // panggil constant constructor
+  var p2 = const Immutable(6, 9); // panggil constant constructor
+  print(p1 == p2); // true
 }
