e599ac2d3e1dc570217351a2e38eee291f4e0c9a
RIOKOWI <achyarrio2004@gmail.com>
2025-09-28 06:32:39 +0700

    INITIALIZER LIST : buat initializer list

---
+++ bin/initializer_list.dart
@@ -0,0 +1,21 @@
+class Customer{
+  String firstName = '';
+  String lastName = '';
+  String fullName = '';
+
+  Customer (this.fullName)
+    : firstName = fullName.split(' ')[0], // initializer list
+      lastName = fullName.split(' ')[1] // initializer list
+      {
+        print('nama customer ini $fullName');
+      }
+  
+}
+
+
+void main()
+{
+  var cust = Customer('Rio Embut');
+  print(cust.firstName);
+  print(cust.lastName);
+}
