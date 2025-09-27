6aa5d96cbf71607806e6902bfea54075e698e15e
RIOKOWI <achyarrio2004@gmail.com>
2025-09-27 07:39:52 +0700

    METHOD EXPRESSION BODY : buat method

---
+++ bin/method_expression_body.dart
@@ -0,0 +1,11 @@
+class Computer {
+  void start() => print('jawir nyalakan komputernya');
+  void shudown() => print('jawir matikan komputernya');
+
+  String update() => 'jawir update komputernya';
+}
+
+
+void main(){
+
+}
