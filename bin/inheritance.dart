eecec76b0f52061938e9584fd013045d7a7d309a
RIOKOWI <achyarrio2004@gmail.com>
2025-09-28 16:48:59 +0700

    INHERITANCE : buat inheritance

---
+++ bin/inheritance.dart
@@ -0,0 +1,17 @@
+class Mpruy{
+  String? name;
+
+  void hallo(String name){
+    print('hallo $name, my name is ${this.name}');
+  }
+}
+
+// inheritance
+class Cungpruy extends Mpruy{
+  // String? address; // boleh tambah ini
+}
+
+void main()
+{
+
+}
