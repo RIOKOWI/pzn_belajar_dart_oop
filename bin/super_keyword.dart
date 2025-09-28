a12aee83bdce805569b97a819efa6beef3cab395
RIOKOWI <achyarrio2004@gmail.com>
2025-09-28 19:58:07 +0700

    SUPER KEYWORD : buat super keyword

--- bin/super_keyword.dart
+++ bin/super_keyword.dart
@@ -1,4 +1,18 @@
+class Bulat{
+  int getUjung(){
+    return 0;
+  }
+}
 
+class Kota extends Bulat{
+  int getUjung(){
+    return 4;
+  }
+
+  int getParentUjung(){
+    return super.getUjung(); // super keyword
+  }
+}
 
 
 
