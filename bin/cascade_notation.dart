c77b08984c140f66bf2a7416307b439107e4ee7b
RIOKOWI <achyarrio2004@gmail.com>
2025-09-28 16:18:24 +0700

    NOTATION CASCADE : tanpa notation cascade

---
+++ bin/cascade_notation.dart
@@ -0,0 +1,15 @@
+class User{
+  // tanpa notation cascade
+  String? username;
+  String? name;
+  String? email;
+}
+
+
+void main() {
+  // tanpa notation cascade
+  var get = User();
+  get.username = 'riokowi';
+  get.name = 'rio achyar';
+  get.email = 'riokowi@gamil.com';
+}
