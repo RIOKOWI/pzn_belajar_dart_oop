ee6d3a93d89f86c5756460c22a18c135c689894e
RIOKOWI <achyarrio2004@gmail.com>
2025-09-28 17:08:40 +0700

    FIELD OVERRIDING : buat dan panggil field overriding

--- bin/field_overriding.dart
+++ bin/field_overriding.dart
@@ -1,3 +1,21 @@
 class Orang {
-  
+  String name = 'Orang';
+
+  void sapa(String name){
+    print('halo $name, nama saya ${this.name}');
+  }
+}
+
+// field overriding
+class OrangLain extends Orang{
+  String name = 'Orang Lain';
+}
+
+
+void main(){
+  var orang = Orang();
+  orang.sapa('rio');
+
+  var orangLain = OrangLain(); // menggunakan field di class OrangLain
+  orangLain.sapa('jono');
 }
