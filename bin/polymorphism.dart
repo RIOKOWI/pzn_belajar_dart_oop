363f7b8ca6b98c5c154320f9968d4f1c1e5554dc
RIOKOWI <achyarrio2004@gmail.com>
2025-09-29 16:45:50 +0700

    POLYMORPHISM : buat inheritance dan panggil polymorphism

--- bin/polymorphism.dart
+++ bin/polymorphism.dart
@@ -1,3 +1,36 @@
+class Karyawan{
+  String? nama;
+  Karyawan(this.nama);
+}
+
+class Atasan extends Karyawan{
+  Atasan(String nama) : super(nama){
+
+  }
+}
+
+class Vp extends Atasan{
+  Vp(String nama) : super(nama){
+
+  }
+}
+
+
+
 void main(){
-  
+
+  Karyawan karyawan = Karyawan('ucup');
+  print(karyawan);
+  print(karyawan.nama);
+
+  karyawan = Atasan('damsuy');// polymorphism
+  print(karyawan); // polymorphism
+  print(karyawan.nama);// polymorphism
+
+  karyawan = Vp('rio');// polymorphism
+  print(karyawan);// polymorphism
+  print(karyawan.nama);// polymorphism
+
+
+
 }
