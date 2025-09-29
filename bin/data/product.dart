66ee5be286210aa04dcc7508b94cf09e6977771e
RIOKOWI <achyarrio2004@gmail.com>
2025-09-30 05:45:30 +0700

    ACCESS MODIFIER : buat access modifier

---
+++ bin/data/product.dart
@@ -0,0 +1,16 @@
+class Produk{
+  int? id;
+  String? name;
+  int? _quantity; // access modifier tidak bisa diakses di luar file
+
+  int? _getQuan(){ // access modifier tidak bisa diakses di luar file
+    return _quantity; // access modifier tidak bisa diakses di luar file
+  }
+}
+void main(){
+  var prod = Produk();
+  prod.id = 1;
+  prod.name = "cunpruy";
+  prod._quantity = 100;
+  prod._getQuan();
+}
