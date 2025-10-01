1d0ecbb3897e47c36d50cfbcb4317b40c562c43a
RIOKOWI <achyarrio2004@gmail.com>
2025-10-01 19:25:43 +0700

    GETTER AND SETTER : panggil getter and setter

--- bin/data/kotak.dart
+++ bin/data/kotak.dart
@@ -2,19 +2,26 @@ class Kotak{
   int _width = 0;
   int _length = 0;
 
-  int get width { // getter
-    return _width;
-  }
+  // int get width { // getter
+  //   return _width;
+  // }
 
-  set width (int value){ // setter
-    _width = value;
-  }
+  // set width (int value){ // setter
+  //   _width = value;
+  // }
 
-  int get length { // getter
-    return _length;
-  }
+  // int get length { // getter
+  //   return _length;
+  // }
 
-  set length (int value){ //setter
-    _length = value;
-  }
+  // set length (int value){ //setter
+  //   _length = value;
+  // }
+
+  int get width => _width; // getter anoymous function
+  set width(int value) => _width = value; // setter anoymous function
+
+  int get length => _length; // getter anoymous function
+  set length(int value) => _length = value; // setter anoymous function
+
 }
--- bin/getter_setter.dart
+++ bin/getter_setter.dart
@@ -1,5 +1,12 @@
 import 'data//kotak.dart';
 
 void main(){
-  var pa
+  var panggil = Kotak();
+  panggil.width = 10000; // setter 
+  print(panggil.width); // getter
+
+  panggil.length = 100; // setter 
+  print(panggil.length); // getter
+
+
 }
