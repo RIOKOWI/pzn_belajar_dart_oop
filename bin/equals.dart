f64993322d41546daeb38ef6f12685239b4ade89
RIOKOWI <achyarrio2004@gmail.com>
2025-10-05 06:31:32 +0700

    EQUALS OPERATOR : sesudah oveeride method equals

--- bin/equals.dart
+++ bin/equals.dart
@@ -4,6 +4,8 @@ void main(){
   var obj1 = Category('1', 'lap');
   var obj2 = Category('1', 'lap');
 
-  print(obj1 == obj2); // (sebelum override method equals operator) false karena di simpan di 2 memori yang berbeda
+  // print(obj1 == obj2); // (sebelum override equals method) false karena di simpan di 2 memori yang berbeda
   print(obj2 == obj2); // true karna di memori yang sama
+
+  print(obj1 == obj2); // (sesudah override equals method) true karena sudah bisa membandingkan value & field yang sama di 2 memoi yang berbeda
 }
