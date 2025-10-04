a9ae3d93994aafc3bb5539f97b5b73019d07aaa9
RIOKOWI <achyarrio2004@gmail.com>
2025-10-05 06:59:59 +0700

    HASHCODE GETTER : test override hashcode method

--- bin/equals.dart
+++ bin/equals.dart
@@ -4,10 +4,14 @@ void main(){
   var obj1 = Category('1', 'lap');
   var obj2 = Category('1', 'lap');
 
+  print('\nEQUALS METHOD');
   // print(obj1 == obj2); // (sebelum override equals method) false karena di simpan di 2 memori yang berbeda
   print(obj2 == obj2); // true karna di memori yang sama
 
   print(obj1 == obj2); // (sesudah override equals method) true karena sudah bisa membandingkan logic yang sama di 2 memoi yang berbeda
 
-  
+  print('\nHASHCODE METHOD');
+  print(obj1.hashCode);
+  print(obj2.hashCode);
+  print(obj1.hashCode == obj2.hashCode);
 }
