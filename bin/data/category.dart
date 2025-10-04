5768e835ceabadf07099fda9a30383192aaa1e9b
RIOKOWI <achyarrio2004@gmail.com>
2025-10-05 06:27:51 +0700

    EQUALS OPERATOR : override equals operator

--- bin/data/category.dart
+++ bin/data/category.dart
@@ -4,7 +4,14 @@ class Category {
 
   Category(this.id, this.name);
 
-  // bool operator (Object == other){
-  //   if 
-  // }
+  bool operator ==(Object other){
+    if (other is Category){
+      if (id != other.id) return false;
+      if (name != other.name) return false;
+
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
