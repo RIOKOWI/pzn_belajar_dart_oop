d20f735a9ae8ae68035bb6364e8c2b91a4a58718
RIOKOWI <achyarrio2004@gmail.com>
2025-10-20 18:44:50 +0700

    STATIC : static field

---
+++ bin/static_field.dart
@@ -0,0 +1,11 @@
+class App {
+  static String name = ''; // bisa diubah 
+  static final String author = 'Rio Achyar';
+}
+
+void main()
+{
+  App.name = 'Cungpruy'; // bisa diubah
+  print(App.name);
+  print(App.author);
+}
