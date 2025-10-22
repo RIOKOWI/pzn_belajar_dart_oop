897aaf9df58ce27c89a0760615d43f931ddc5c5a
RIOKOWI <achyarrio2004@gmail.com>
2025-10-22 19:49:52 +0700

    EXCEPTION : tes

--- bin/exception.dart
+++ bin/exception.dart
@@ -1,10 +1,14 @@
 class Validation {
   static void validate(String username, String password){
     if(username == ""){
-      throw Exception("username is empty");
+      throw Exception("username is empty"); // exception
     }
     else if(password == ""){
-      throw Exception("password is empty");
+      throw Exception("password is empty"); // exception
     }
   }
+}
+
+void main(){
+  Validation.validate("", "");
 }
