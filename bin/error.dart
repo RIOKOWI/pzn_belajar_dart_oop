f02baf1ccca2bf61891a50ba90f3638e25837f04
RIOKOWI <achyarrio2004@gmail.com>
2025-12-05 07:41:59 +0700

    ERROR: test code error

---
+++ bin/error.dart
@@ -0,0 +1,11 @@
+void main(){
+  var list = [1, 2, 3];
+  // error
+    print(list[10]);
+
+  // if(list.length > 10){
+  //   print(list[10]);
+  // }
+
+  // kalo ada error bijak nya diperbaiki
+}
